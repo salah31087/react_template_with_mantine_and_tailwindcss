0f1f82c8f9457a696a07e6224493a0f74bb48916
Belaid Salah eddine <salahbelaid3108@gmail.com>
2024-10-11 14:04:12 +0100

    improve dark mode function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Button } from '@mantine/core'
 import React from 'react'
-import SwitchDarkMode from './utils/SwitchDarkMode'
 
 export default function App() {
   return (
     <div>
-      <SwitchDarkMode />
+
     </div>
   )
 }
